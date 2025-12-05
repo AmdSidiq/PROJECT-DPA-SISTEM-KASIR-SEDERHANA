6f1cca328d6e37b8ef679ccf7a7b019526554cae
AmdSidiq <ahmadsidiq796@gmail.com>
2025-12-05 21:45:35 +0700

    Nambahin barang di list

--- Sistem Kasir Sederhana.py
+++ Sistem Kasir Sederhana.py
@@ -1,2 +1,10 @@
-list_barang = []
+list_barang = [
+    {"nomor": 1, "nama": "Air Minum", "harga": 5000},
+    {"nomor": 2, "nama": "Roti", "harga": 12000},
+    {"nomor": 3, "nama": "Cookie", "harga": 15000},
+    {"nomor": 4, "nama": "Es Krim", "harga": 3500},
+    {"nomor": 5, "nama": "Mie Instan", "harga": 5000},
+    {"nomor": 6, "nama": "Galon", "harga": 50000}
+]
+
 riwayat = []
