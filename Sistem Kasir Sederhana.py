ef4a9dc79f08490ec96e2207c7bb111c37d8ad2c
AmdSidiq <ahmadsidiq796@gmail.com>
2025-12-06 01:05:41 +0700

    Fungsi tambah barang

--- Sistem Kasir Sederhana.py
+++ Sistem Kasir Sederhana.py
@@ -1,10 +1,10 @@
-list_barang = [
+barang_list = [
     {"nomor": 1, "nama": "Air Minum", "harga": 5000},
     {"nomor": 2, "nama": "Roti", "harga": 12000},
     {"nomor": 3, "nama": "Cookie", "harga": 15000},
     {"nomor": 4, "nama": "Es Krim", "harga": 3500},
     {"nomor": 5, "nama": "Mie Instan", "harga": 5000},
-    {"nomor": 6, "nama": "Galon", "harga": 50000}
+    {"nomor": 6, "nama": "Galon", "harga": 50000},
     {"nomor": 7, "nama": "Rokok", "harga": 30000},
     {"nomor": 8, "nama": "Terminal colokan", "harga": 15000},
     {"nomor": 9, "nama": "Minyak", "harga": 20000},
@@ -26,4 +26,20 @@ def input_angka(pesan):
             angka = int(input(pesan))
             return angka
         except:
-            print("Masukkan angka yang benar!") 
+            print("Masukkan angka yang benar!")
+
+def tambah_barang():
+    while True:
+        nama = input("Nama barang: ")
+        if nama.strip() == "":
+            print("Nama tidak boleh kosong!")
+        else:
+            harga = input_angka("Harga barang: Rp.")
+            if harga > 0:
+                nomor_baru = len(barang_list) + 1
+                barang_list.append({"nomor": nomor_baru, "nama": nama, "harga": harga})
+                print("Barang telah ditambahkan!")
+                print("--------------------")
+                break
+            else:
+                print("Nominal harga harus diatas 0!\n")
