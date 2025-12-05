2a588ef9e954f1dd2a148835b53e9932dc4a69e0
bungawlnsr <wulansarib6@gmail.com>
2025-12-05 21:55:43 +0700

    Add new products to the cash register system

--- Sistem Kasir Sederhana.py
+++ Sistem Kasir Sederhana.py
@@ -5,6 +5,11 @@ list_barang = [
     {"nomor": 4, "nama": "Es Krim", "harga": 3500},
     {"nomor": 5, "nama": "Mie Instan", "harga": 5000},
     {"nomor": 6, "nama": "Galon", "harga": 50000}
+    {{"nomor": 7, "nama": "Rokok", "harga": 30000},
+    {"nomor": 8, "nama": "Terminal colokan", "harga": 15000},
+    {"nomor": 9, "nama": "Minyak", "harga": 20000},
+    {"nomor": 10, "nama": "Odol", "harga": 12000},
+    {"nomor": 11, "nama": "Sikat Gigi", "harga": 10000}
 ]
 
 riwayat = []
