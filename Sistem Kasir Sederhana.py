3b53f6b6c0378bebbf8db52cf7d2b97ace4f5dcc
AmdSidiq <ahmadsidiq796@gmail.com>
2025-12-05 22:47:46 +0700

    Fungsi input angka biar ga bikin try except terus

--- Sistem Kasir Sederhana.py
+++ Sistem Kasir Sederhana.py
@@ -5,7 +5,7 @@ list_barang = [
     {"nomor": 4, "nama": "Es Krim", "harga": 3500},
     {"nomor": 5, "nama": "Mie Instan", "harga": 5000},
     {"nomor": 6, "nama": "Galon", "harga": 50000}
-    {{"nomor": 7, "nama": "Rokok", "harga": 30000},
+    {"nomor": 7, "nama": "Rokok", "harga": 30000},
     {"nomor": 8, "nama": "Terminal colokan", "harga": 15000},
     {"nomor": 9, "nama": "Minyak", "harga": 20000},
     {"nomor": 10, "nama": "Odol", "harga": 12000},
@@ -19,3 +19,11 @@ def tampilkan_barang():
     for i in barang_list:
         print(i["nomor"], i["nama"], i["harga"])
     print("--------------------")
+
+def input_angka(pesan):
+    while True:
+        try:
+            angka = int(input(pesan))
+            return angka
+        except:
+            print("Masukkan angka yang benar!") 
