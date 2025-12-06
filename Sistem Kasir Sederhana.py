ccc0b4131e2291e8324f378e290ed099593aab8b
AmdSidiq <ahmadsidiq796@gmail.com>
2025-12-07 00:28:19 +0700

    kebanyakan barangnya

--- Sistem Kasir Sederhana.py
+++ Sistem Kasir Sederhana.py
@@ -1,15 +1,10 @@
 barang_list = [
-    {"nomor": 1, "nama": "Air Minum", "harga": 5000},
+    {"nomor": 1, "nama": "Rokok", "harga": 30000},
     {"nomor": 2, "nama": "Roti", "harga": 12000},
-    {"nomor": 3, "nama": "Cookie", "harga": 15000},
-    {"nomor": 4, "nama": "Es Krim", "harga": 3500},
-    {"nomor": 5, "nama": "Mie Instan", "harga": 5000},
-    {"nomor": 6, "nama": "Galon", "harga": 50000},
-    {"nomor": 7, "nama": "Rokok", "harga": 30000},
-    {"nomor": 8, "nama": "Terminal colokan", "harga": 15000},
-    {"nomor": 9, "nama": "Minyak", "harga": 20000},
-    {"nomor": 10, "nama": "Odol", "harga": 12000},
-    {"nomor": 11, "nama": "Sikat Gigi", "harga": 10000}
+    {"nomor": 3, "nama": "Minyak", "harga": 17500},
+    {"nomor": 4, "nama": "Mie Instan", "harga": 3000},
+    {"nomor": 5, "nama": "Odol", "harga": 12000},
+    {"nomor": 6, "nama": "Sikat Gigi", "harga": 10000}
 ]
 
 riwayat = []
